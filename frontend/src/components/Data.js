756ef112f8898d4cd9aac7484d13700cacb21051
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2022-01-07 21:45:33 -0600

    data working unorganized need to open jupyer

--- frontend/src/components/Data.js
+++ frontend/src/components/Data.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const Data = ({ data }) => {
   return (
@@ -7,13 +8,23 @@ const Data = ({ data }) => {
       <Card className='my-3 p-3 rounded'>
         <Card.Body>
           <Card.Title as='div'>
+            <Link to={`${data.url}`}>
+              <Card.Img
+                src='https://source.unsplash.com/random/?technology'
+                variant='top'
+              />
+            </Link>
+
+            {/*  */}
             <strong>{data.name}</strong>
+            <small>{data.description}</small>
           </Card.Title>
 
-          <Card.Text as='div'>
-            value={data.rating}
-            text={` ${data.description} `}
-          </Card.Text>
+          <Card.Text as='div'>{` ${data.host} `}</Card.Text>
+          <Card.Text as='div'>{` ${data.rating} `}</Card.Text>
+          <Card.Text as='div'>{` ${data.lectures} `}</Card.Text>
+          <Card.Text as='div'>{` ${data.level} `}</Card.Text>
+          <Card.Text as='a'>{` ${data.url} `}</Card.Text>
 
           <Card.Text as='h3'>${data.price}</Card.Text>
         </Card.Body>
